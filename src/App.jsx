7f05f980ca15550247ca90975033f9207c66b8e8
Mikhail Khapkov <xmeloff12326@gmail.com>
2025-09-22 18:36:34 +0300

    added new library and new route in app

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import PoseDetectionPage from './components/pose_capture/PoseDetectionPage.jsx';
 import XrModelContainer from './components/xr/XrModelContainer.jsx';
+import CubeContainer from './components/xr/CubeContainer.jsx';
 
 
-// <Route path="/pose" element={<PoseDetectionPage />} />
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<XrModelContainer />} />
+      <Route path="/" element={<CubeContainer />} />
+      <Route path="/xr" element={<XrModelContainer />} />
+      <Route path="/pose" element={<PoseDetectionPage />} />
     </Routes>
   );
 }
